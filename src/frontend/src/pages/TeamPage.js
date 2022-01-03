2014bf67c32a940eced9411ac487888e6964bb49
Vidheesh Nacode <vidheeshnacode@hedwig.local>
2022-01-03 00:10:39 -0500

    Changes for deployment

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -17,9 +17,8 @@ export const TeamPage = () => {
     useEffect(
         () => {
             const fetchMatches = async () => {
-                const response = await fetch(`http://localhost:8080/team/${teamName}`);
+                const response = await fetch(`${process.env.REACT_APP_API_ROOT_URL}/team/${teamName}`);
                 const data = await response.json();
-                console.log(data);
                 setTeam(data);
             };
             fetchMatches();
@@ -50,7 +49,7 @@ export const TeamPage = () => {
         <h3>Latest Matches</h3>
           <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
         </div>
-        {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match}/>)}
+        {team.matches.slice(1).map(match => <MatchSmallCard key={match.id} teamName={team.teamName} match={match}/>)}
         <div className="more-link">
         <Link to={`/team/${teamName}/matches/${process.env.REACT_APP_DATA_END_YEAR}`}>More ></Link>
         </div>
